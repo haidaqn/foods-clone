0a95656f00a92f8dccc9c204eaa51a1ce66ff3ad
Phương Hải Đăng <93462885+haidaqn@users.noreply.github.com>
2022-11-24 15:09:51 +0700

    haidang

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Title() {
     return (
         <div className="absolute h-full w-full text-gray-200 h-screen bg-black/40 flex flex-col justify-center">
-            <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+            <h1 className="px-4 text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl ">
                 The <span className="text-orange-500">Best</span>
             </h1>
             <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
